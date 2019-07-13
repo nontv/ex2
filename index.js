758ab3f55ceeec874c37b0874514c6b2b3422c5e
Thamnoon boonmai <58143420@g.cmru.ac.th>
2019-07-13 14:14:48 +0700

    เพิ่มหน้าtest

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('thamnoon non'))
 app.get('/Adout', (req, res) => res.send('หน้า เกี่ยวกับ'))
 app.get('/contact', (req, res) => res.send('หน้า ติอต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message : " test "})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
  
